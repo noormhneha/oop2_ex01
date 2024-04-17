6947d5ce1238586c59551ec408515e40d0747358
noormhaneha <nmhaneha@gmail.com>
2024-04-17 17:59:06 +0300

    some comments

--- src/DuplicatedShape.cpp
+++ src/DuplicatedShape.cpp
@@ -1,30 +1,44 @@
 #include "DuplicatedShape.h"
 
 DuplicatedShape::DuplicatedShape(std::shared_ptr<Shape> original_shape, int times)
-    : m_originalShape(original_shape->clone())
-    , m_firstOriginalShape(original_shape)
-    , m_times(times) {}
+        : m_originalShape(original_shape->clone()), // Cloning the original shape
+          m_firstOriginalShape(original_shape),     // Storing a pointer to the original shape
+          m_times(times) {}                         // Storing the number of times to duplicate
 
+// ------------------------------------------------------------------
 DuplicatedShape::DuplicatedShape(const DuplicatedShape& other)
-    : m_originalShape(other.m_originalShape->clone())
-    , m_firstOriginalShape(other.m_firstOriginalShape)
-    , m_times(other.m_times) {}
+        : m_originalShape(other.m_originalShape->clone()), // Cloning the original shape
+          m_firstOriginalShape(other.m_firstOriginalShape), // Copying the pointer to the original shape
+          m_times(other.m_times) {}                        // Copying the number of times to duplicate
 
+// ------------------------------------------------------------------
+// Clone function to create a copy of the DuplicatedShape object
 std::shared_ptr<Shape> DuplicatedShape::clone() const { return std::make_shared<DuplicatedShape>(*this); }
 
+// ------------------------------------------------------------------
+// Function to print the name of the shape and the number of times it's duplicated
 void DuplicatedShape::nameOfShape() const {
-    std::cout << m_times << " * (";
-    m_originalShape->nameOfShape();
-    std::cout << ")";
+    std::cout << m_times << " * (";       // Print the duplication factor
+    m_originalShape->nameOfShape();       // Print the name of the original shape
+    std::cout << ")";                     // Closing parenthesis
 }
 
+// ------------------------------------------------------------------
+// Function to draw the shape multiple times
 void DuplicatedShape::draw(double size) const { m_originalShape->draw(m_times); }
 
+// ------------------------------------------------------------------
+// Function to set a new size for the shape
 void DuplicatedShape::setNewSize(double factor) {
-    DuplicatedShape duplicated(*this);
-    duplicated.m_originalShape->setNewSize(factor);
-    this->m_originalShape = duplicated.m_originalShape;
+    DuplicatedShape duplicated(*this);                   // Create a duplicate of the current object
+    duplicated.m_originalShape->setNewSize(factor);      // Set the new size for the duplicated shape
+    this->m_originalShape = duplicated.m_originalShape;  // Update the original shape pointer to the new duplicated shape
 }
 
+// ------------------------------------------------------------------
+// Function to get the original shape
 std::shared_ptr<Shape> DuplicatedShape::getShape() const { return m_originalShape; }
+
+// ------------------------------------------------------------------
+// Function to set the size of the shape
 void DuplicatedShape::setSize(double factor) { m_originalShape->setNewSize(factor); }
--- src/StackedShape.cpp
+++ src/StackedShape.cpp
@@ -1,19 +1,23 @@
 #include "StackedShape.h"
 
 StackedShape::StackedShape(std::shared_ptr<Shape> shape1, std::shared_ptr<Shape> shape2)
-    : m_aboveShape(shape1->clone())
-    , m_belowShape(shape2->clone())
-    , m_firstAboveShape(shape1)
-    , m_firstBelowShape(shape2) {}
+        : m_aboveShape(shape1->clone()),        // Cloning the first shape
+          m_belowShape(shape2->clone()),        // Cloning the second shape
+          m_firstAboveShape(shape1),            // Storing a pointer to the first shape
+          m_firstBelowShape(shape2) {}          // Storing a pointer to the second shape
 
-StackedShape::StackedShape(const StackedShape& other) 
-    : m_aboveShape(other.m_aboveShape->clone())
-    , m_belowShape(other.m_belowShape->clone())
-    , m_firstAboveShape(other.m_firstAboveShape)
-    , m_firstBelowShape(other.m_firstBelowShape) {}
+// ------------------------------------------------------------------
+StackedShape::StackedShape(const StackedShape& other)
+        : m_aboveShape(other.m_aboveShape->clone()),   // Cloning the above shape
+          m_belowShape(other.m_belowShape->clone()),   // Cloning the below shape
+          m_firstAboveShape(other.m_firstAboveShape),  // Copying the pointer to the first shape
+          m_firstBelowShape(other.m_firstBelowShape) {}// Copying the pointer to the second shape
 
+
+// ------------------------------------------------------------------
 std::shared_ptr<Shape> StackedShape::clone() const { return std::make_shared<StackedShape>(*this); }
 
+// ------------------------------------------------------------------
 void StackedShape::nameOfShape() const {
     std::cout << "(";
     m_aboveShape->nameOfShape();
@@ -22,6 +26,7 @@ void StackedShape::nameOfShape() const {
     std::cout << ")";
 }
 
+// ------------------------------------------------------------------
 void StackedShape::draw(double times) const {
     for (size_t i = 0; i < times; i++) {
         m_aboveShape->draw(1);
@@ -29,6 +34,7 @@ void StackedShape::draw(double times) const {
     }
 }
 
+// ------------------------------------------------------------------
 void StackedShape::setNewSize(double factor) {
     StackedShape stacked(*this);
     stacked.m_aboveShape->setNewSize(factor);
@@ -37,8 +43,13 @@ void StackedShape::setNewSize(double factor) {
     this->m_belowShape = stacked.m_belowShape;
 }
 
+// ------------------------------------------------------------------
+// Function to get the pointer to the shape [above|below]
 std::shared_ptr<Shape> StackedShape::getAboveShape() const { return m_firstAboveShape;}
 std::shared_ptr<Shape> StackedShape::getBelowShape() const { return m_firstBelowShape; }
+
+// ------------------------------------------------------------------
+// Function to set the size of the shape [above|below]
 void StackedShape::setSizeAbove(double factor) { m_aboveShape->setNewSize(factor); }
 void StackedShape::setSizeBelow(double factor) { m_belowShape->setNewSize(factor); }
 
